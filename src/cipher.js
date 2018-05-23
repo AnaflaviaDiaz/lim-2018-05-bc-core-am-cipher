5206a8132d6a1a370fb5a8f7fb9e90297a8da5b9
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-22 20:29:56 -0500

    cipher con logica mejorada

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,34 @@
 window.cipher = {
   encode: encode,
   decode: decode
-  // encode: (offset, string) => {}
 };
 
 function encode(offset, text) {
+  var encodeResult = "";
   for (let i = 0; i < text.length; i++) {
-    encodeResult += String.fromCharCode(text.charCodeAt(i) + offset);
+    var textChar = text.charCodeAt(i);
+    if (textChar >= 97 && textChar <= 122) {
+      encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
+    } else if (textChar >= 65 && textChar <= 90) {
+      encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+    } else {
+      encodeResult += String.fromCharCode((textChar));
+    }
   }
   console.log(encodeResult);
 }
 
 function decode(offset, text) {
+  var decodeResult = "";
   for (let i = 0; i < text.length; i++) {
-    decodeResult += String.fromCharCode(text.charCodeAt(i) - offset);
+    var textChar = text.charCodeAt(i);
+    if (textChar >= 97 && textChar <= 122) {
+      decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
+    } else if (textChar >= 65 && textChar <= 90) {
+      decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
+    } else {
+      decodeResult += String.fromCharCode(textChar);
+    }
   }
   console.log(decodeResult);
 }
