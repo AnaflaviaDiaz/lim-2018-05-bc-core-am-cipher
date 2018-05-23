81f9e59f8e46bbd06ad77ae38ecbb272bb5db007
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-22 19:46:48 -0500

    enlazando cipher con index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,19 @@
 window.cipher = {
-  encode: (offset, string) => {}
+  encode: encode,
+  decode: decode
+  // encode: (offset, string) => {}
 };
 
+function encode(offset, text) {
+  for (let i = 0; i < text.length; i++) {
+    encodeResult += String.fromCharCode(text.charCodeAt(i) + offset);
+  }
+  console.log(encodeResult);
+}
+
+function decode(offset, text) {
+  for (let i = 0; i < text.length; i++) {
+    decodeResult += String.fromCharCode(text.charCodeAt(i) - offset);
+  }
+  console.log(decodeResult);
+}
--- src/index.js
+++ src/index.js
@@ -1,61 +1,39 @@
 var encodeText, decodeText, offset;
 var encodeResult = '';
 var decodeResult = '';
-// document.getElementById("btnOffset").addEventListener("click", offset);
 
-// function offset() {
-//   var offset = document.getElementById("txtOffset");
-//   document.getElementById('formOffset').style.display = 'none';
-//   document.getElementById('encriptado').style.display = 'list-item';
-// }
-
-function encode() {
+document.getElementById("btnEncrypted").addEventListener("click", function() {
   offset = document.getElementById("txtOffset").value;
   offset = parseInt(offset);
   encodeText = document.getElementById("txtEncrypted").value;
-  console.log(offset, encodeText);
-  for (let i = 0; i < encodeText.length; i++) {
-    encodeResult += String.fromCharCode(encodeText.charCodeAt(i) + offset);
-  }
-  console.log(encodeResult);
-}
+  cipher.encode(offset, encodeText);
+});
 
-function decode() {
+document.getElementById("btnDecrypted").addEventListener("click", function() {
   offset = document.getElementById("txtOffset").value;
   offset = parseInt(offset);
-  decodeText = document.getElementById("txtDecryted").value;
-  console.log(offset, decodeText);
-  for (let i = 0; i < decodeText.length; i++) {
-    decodeResult += String.fromCharCode(decodeText.charCodeAt(i) - offset);
-  }
-  console.log(decodeResult);
-}
-
-
-
-
-// document.getElementById('txtEncrypted').style.display = 'list-item';
-// document.getElementById('txtDecryted').style.display = 'list-item';
-// document.getElementById('btnEncryted').style.display = 'list-item';
-// document.getElementById('btnDecryted').style.display = 'list-item';
-// var txtOffset = document.getElementById("txtOffset");
-// var btnOffset = document.getElementById("btnOffset");
+  decodeText = document.getElementById("txtDecrypted").value;
+  cipher.decode(offset, decodeText);
+});
 
 // function encode() {
-//   var txtEncrypted = document.getElementById("txtEncrypted");
-//   var btnEncryted = document.getElementById("btnEncrypted");
+//   offset = document.getElementById("txtOffset").value;
+//   offset = parseInt(offset);
+//   encodeText = document.getElementById("txtEncrypted").value;
+//   console.log(offset, encodeText);
+//   for (let i = 0; i < encodeText.length; i++) {
+//     encodeResult += String.fromCharCode(encodeText.charCodeAt(i) + offset);
+//   }
+//   console.log(encodeResult);
 // }
 
 // function decode() {
-//   var txtDecryted = document.getElementById("txtDecrypted");
-//   var btnDecryted = document.getElementById("btnDecryted");
+//   offset = document.getElementById("txtOffset").value;
+//   offset = parseInt(offset);
+//   decodeText = document.getElementById("txtDecryted").value;
+//   console.log(offset, decodeText);
+//   for (let i = 0; i < decodeText.length; i++) {
+//     decodeResult += String.fromCharCode(decodeText.charCodeAt(i) - offset);
+//   }
+//   console.log(decodeResult);
 // }
-// function caesar(texto, clave) {
-//     var output = "";
-//     clave = parseInt(clave);
-//     for (var i = 0; i < texto.length; i++) {
-//         output += String.fromCharCode(texto.charCodeAt(i) + clave);
-//     }
-//     return output;
-// }
-
