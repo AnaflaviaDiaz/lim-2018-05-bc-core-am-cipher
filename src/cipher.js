7ae0001f77b7dcc333dd3de0485c57611e17d811
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-23 10:33:28 -0500

    arreglando algunas cosas

--- src/cipher.js
+++ src/cipher.js
@@ -8,15 +8,18 @@ function encode(offset, text) {
   for (let i = 0; i < text.length; i++) {
     var textChar = text.charCodeAt(i);
     if (textChar >= 97 && textChar <= 122) {
+      //minuscula
       encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
     } else if (textChar >= 65 && textChar <= 90) {
+      //Mayuscula
       encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
     } else {
+      //signos o espacios
       encodeResult += String.fromCharCode((textChar));
     }
   }
-  return encodeResult;
-  // console.log(encodeResult);
+  // return encodeResult;
+  console.log(encodeResult);
 }
 
 function decode(offset, text) {
@@ -24,13 +27,16 @@ function decode(offset, text) {
   for (let i = 0; i < text.length; i++) {
     var textChar = text.charCodeAt(i);
     if (textChar >= 97 && textChar <= 122) {
+      //minuscula
       decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
     } else if (textChar >= 65 && textChar <= 90) {
+      //mayuscula
       decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
     } else {
+      // signos espacios
       decodeResult += String.fromCharCode(textChar);
     }
   }
-  return decodeResult;
-  // console.log(decodeResult);
+  // return decodeResult;
+  console.log(decodeResult);
 }
--- src/index.js
+++ src/index.js
@@ -13,25 +13,3 @@ document.getElementById("btnDecrypted").addEventListener("click", function() {
   decodeText = document.getElementById("txtDecrypted").value;
   cipher.decode(offset, decodeText);
 });
-
-// function encode() {
-//   offset = document.getElementById("txtOffset").value;
-//   offset = parseInt(offset);
-//   encodeText = document.getElementById("txtEncrypted").value;
-//   console.log(offset, encodeText);
-//   for (let i = 0; i < encodeText.length; i++) {
-//     encodeResult += String.fromCharCode(encodeText.charCodeAt(i) + offset);
-//   }
-//   console.log(encodeResult);
-// }
-
-// function decode() {
-//   offset = document.getElementById("txtOffset").value;
-//   offset = parseInt(offset);
-//   decodeText = document.getElementById("txtDecryted").value;
-//   console.log(offset, decodeText);
-//   for (let i = 0; i < decodeText.length; i++) {
-//     decodeResult += String.fromCharCode(decodeText.charCodeAt(i) - offset);
-//   }
-//   console.log(decodeResult);
-// }
