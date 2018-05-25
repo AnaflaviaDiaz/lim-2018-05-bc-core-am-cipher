ef95f283be84c4917ad1f187d0a28bfed01b5262
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-24 21:17:42 -0500

    cambiando encriptado

--- src/cipher.js
+++ src/cipher.js
@@ -8,38 +8,32 @@ function encode(offset, text) {
   var encodeResult = "";
   for (let i = 0; i < text.length; i++) {
     var textChar = text.charCodeAt(i);
-    if (textChar >= 97 && textChar <= 122) {
-      //minuscula
-      encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-    } else if (textChar >= 65 && textChar <= 90) {
-      //Mayuscula
-      encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-    } else {
-      //signos o espacios
+    if (textChar >= 97 && textChar <= 122) {  //Minuscula
+      // encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
+      encodeResult += String.fromCharCode(textChar + offset % 26);
+    } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
+      // encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+      encodeResult += String.fromCharCode(textChar + offset % 26);
+    } else {  //signos o espacios
       encodeResult += String.fromCharCode((textChar));
     }
   }
   return encodeResult;
-  // console.log(encodeResult);
 }
 
 function decode(offset, text) {
   var decodeResult = "";
   for (let i = 0; i < text.length; i++) {
     var textChar = text.charCodeAt(i);
-    if (textChar >= 97 && textChar <= 122) {
-      //minuscula
+    if (textChar >= 97 && textChar <= 122) {  //minuscula
       decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
-    } else if (textChar >= 65 && textChar <= 90) {
-      //mayuscula
+    } else if (textChar >= 65 && textChar <= 90) {  //mayuscula
       decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
-    } else {
-      // signos espacios
+    } else {  // signos espacios
       decodeResult += String.fromCharCode(textChar);
     }
   }
   return decodeResult;
-  // console.log(decodeResult);
 }
 
 function createCipherWithOffset() {}
--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ var encodeText, decodeText, offset;
 document.getElementById("btnEncrypted").addEventListener("click", function() {
   offset = document.getElementById("txtOffset").value;
   encodeText = document.getElementById("txtEncrypted").value;
-  cipher.encode(offset, encodeText);
+  document.getElementById("result").innerHTML = cipher.encode(offset, encodeText);
 });
 
 document.getElementById("btnDecrypted").addEventListener("click", function() {
   offset = document.getElementById("txtOffset").value;
   decodeText = document.getElementById("txtEncrypted").value;
-  cipher.decode(offset, decodeText);
+  document.getElementById("result").innerHTML = cipher.decode(offset, decodeText);
 });
 
 document.getElementById("clear").addEventListener("click", function() {
   document.getElementById("txtOffset").value = "";
   document.getElementById("txtEncrypted").value = "";
+  document.getElementById("result").innerHTML = "";
 });
