73bea19792d9108f89619df8829a9e16fa39f422
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-25 09:41:31 -0500

    cambiando algoritmo

--- src/cipher.js
+++ src/cipher.js
@@ -9,11 +9,11 @@ function encode(offset, text) {
   for (let i = 0; i < text.length; i++) {
     var textChar = text.charCodeAt(i);
     if (textChar >= 97 && textChar <= 122) {  //Minuscula
-      // encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-      encodeResult += String.fromCharCode(textChar + offset % 26);
+      encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
+      // encodeResult += String.fromCharCode(textChar + offset % 26);
     } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
-      // encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-      encodeResult += String.fromCharCode(textChar + offset % 26);
+      encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+      // encodeResult += String.fromCharCode(textChar + offset % 26);
     } else {  //signos o espacios
       encodeResult += String.fromCharCode((textChar));
     }
