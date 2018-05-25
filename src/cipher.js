72bbe01ac1afbedef9e7f7df3fb82674ea1a7e72
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-25 09:46:54 -0500

    cambiando algoritmo

--- src/cipher.js
+++ src/cipher.js
@@ -7,33 +7,41 @@ window.cipher = {
 function encode(offset, text) {
   var encodeResult = "";
   for (let i = 0; i < text.length; i++) {
-    var textChar = text.charCodeAt(i);
-    if (textChar >= 97 && textChar <= 122) {  //Minuscula
-      encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-      // encodeResult += String.fromCharCode(textChar + offset % 26);
-    } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
-      encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-      // encodeResult += String.fromCharCode(textChar + offset % 26);
-    } else {  //signos o espacios
-      encodeResult += String.fromCharCode((textChar));
-    }
+    encodeResult += String.fromCharCode(text.charCodeAt(i) + offset);
   }
   return encodeResult;
+  // for (let i = 0; i < text.length; i++) {
+  //   var textChar = text.charCodeAt(i);
+  //   if (textChar >= 97 && textChar <= 122) {  //Minuscula
+  //     encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
+  //     // encodeResult += String.fromCharCode(textChar + offset % 26);
+  //   } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
+  //     encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+  //     // encodeResult += String.fromCharCode(textChar + offset % 26);
+  //   } else {  //signos o espacios
+  //     encodeResult += String.fromCharCode((textChar));
+  //   }
+  // }
+  // return encodeResult;
 }
 
 function decode(offset, text) {
   var decodeResult = "";
   for (let i = 0; i < text.length; i++) {
-    var textChar = text.charCodeAt(i);
-    if (textChar >= 97 && textChar <= 122) {  //minuscula
-      decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
-    } else if (textChar >= 65 && textChar <= 90) {  //mayuscula
-      decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
-    } else {  // signos espacios
-      decodeResult += String.fromCharCode(textChar);
-    }
+    decodeResult += String.fromCharCode(text.charCodeAt(i) - offset);
   }
   return decodeResult;
+  // for (let i = 0; i < text.length; i++) {
+  //   var textChar = text.charCodeAt(i);
+  //   if (textChar >= 97 && textChar <= 122) {  //minuscula
+  //     decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
+  //   } else if (textChar >= 65 && textChar <= 90) {  //mayuscula
+  //     decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
+  //   } else {  // signos espacios
+  //     decodeResult += String.fromCharCode(textChar);
+  //   }
+  // }
+  // return decodeResult;
 }
 
 function createCipherWithOffset() {}
