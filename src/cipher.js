f89cec9bdccb92aba0f274cfb37aabb8887fe888
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-28 12:45:30 -0500

    al fin funciona el encriptar

--- src/cipher.js
+++ src/cipher.js
@@ -20,10 +20,8 @@ function encode(offset, text) {
     if (alphabet.includes(charter)) {
       if (textChar >= 97 && textChar <= 122) {  //Minuscula
         encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-        // encodeResult += String.fromCharCode(textChar + offset % 26);
       } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
         encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-        // encodeResult += String.fromCharCode(textChar + offset % 26);
       }
     } else encodeResult += String.fromCharCode((textChar));
   }
@@ -55,41 +53,5 @@ function decode(offset, text) {
   }
   return decodeResult;
 }
-
-// function encode(offset, text) {
-//   var encodeResult = "";
-//   // var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   for (let i = 0; i < text.length; i++) {
-//     var textChar = text.charCodeAt(i);
-
-
-//     if (textChar >= 97 && textChar <= 122) {  //Minuscula
-//       // encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-//       encodeResult += String.fromCharCode(textChar + offset % 26);
-//     } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
-//       // encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-//       encodeResult += String.fromCharCode(textChar + offset % 26);
-//     } else {  //signos o espacios
-//       encodeResult += String.fromCharCode((textChar));
-//     }
-//   }
-//   return encodeResult;
-// }
-
-// function decode(offset, text) {
-//   var decodeResult = "";
-//   // var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   for (let i = 0; i < text.length; i++) {
-//     var textChar = text.charCodeAt(i);
-//     if (textChar >= 97 && textChar <= 122) {  //minuscula
-//       decodeResult += String.fromCharCode((textChar - 97 - offset + 26) % 26 + 97);
-//     } else if (textChar >= 65 && textChar <= 90) {  //mayuscula
-//       decodeResult += String.fromCharCode((textChar - 65 - offset + 26) % 26 + 65);
-//     } else {  // signos espacios
-//       decodeResult += String.fromCharCode(textChar);
-//     }
-//   }
-//   return decodeResult;
-// }
 
 function createCipherWithOffset() { }
