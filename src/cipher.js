915dd45b7f6af3da225515cac02abab0599302bf
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-28 19:21:36 -0500

    ecma6

--- src/cipher.js
+++ src/cipher.js
@@ -1,57 +1,63 @@
 window.cipher = {
-  encode: encode,
-  decode: decode,
-  createCipherWithOffset: createCipherWithOffset
-};
+  encode(offset, text) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let encodeResult = "";
 
-/*  function alalalala(offset){
-  retun {
-    decode:function(offset,string){}
-    encode:function(offset,string){}
-  }
-} */
+    for (let i = 0; i < text.length; i++) {
+      let textChar = text.charCodeAt(i);
+      let charter = text.charAt(i);
 
-function encode(offset, text) {
-  var encodeResult = "";
-  var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for (let i = 0; i < text.length; i++) {
-    var textChar = text.charCodeAt(i);
-    var charter = text.charAt(i);
-    if (alphabet.includes(charter)) {
-      if (textChar >= 97 && textChar <= 122) {  //Minuscula
-        encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-      } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
-        encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+      if (alphabet.includes(charter)) {
+        if (textChar >= 97 && textChar <= 122 ) {
+          encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
+        }
+        else if (textChar >= 65 && textChar <= 90) {
+          encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+        }
       }
-    } else encodeResult += String.fromCharCode((textChar));
-  }
-  return encodeResult;
-}
+      else encodeResult += String.fromCharCode(textChar);
+    }
 
-function decode(offset, text) {
-  var decodeResult = "";
-  var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for (let i = 0; i < text.length; i++) {
-    var textChar = text.charCodeAt(i); //numero ascii
-    var charter = text.charAt(i); // cada caracter
-    if (alphabet.includes(charter)) {
-      if (textChar >= 97 && textChar <= 122) {  //Minuscula
-        if ((textChar - offset) < 97 && (textChar - offset % 26) < 97) {
-          var differenceMinus = 97 - (textChar - offset % 26);
-          decodeResult += String.fromCharCode(123 - differenceMinus);
-        } else decodeResult += String.fromCharCode(textChar - offset % 26);
+    return encodeResult;
+  },
+  decode(offset, text) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let decodeResult = "";
 
-      } else if (textChar >= 65 && textChar <= 90) {  //Mayuscula
-        if ((textChar - offset) < 65 && (textChar - offset % 26) < 65) {
-          var differenceMayus = 65 - (textChar - offset % 26);
-          decodeResult += String.fromCharCode(91 - differenceMayus);
-        } else decodeResult += String.fromCharCode(textChar - offset % 26);
+    for (let i = 0; i < text.length; i++) {
+      let textChar = text.charCodeAt(i);
+      let charter = text.charAt(i);
+
+      if (alphabet.includes(charter)) {
+        if (textChar >= 97 && textChar <= 122) {
+          if ((textChar - offset) < 97 && (textChar - offset % 26) < 97) {
+            let differenceMinus = 97 - (textChar - offset % 26);
+            decodeResult += String.fromCharCode(123 - differenceMinus);
+          }
+          else decodeResult += String.fromCharCode(textChar - offset % 26);
+        }
+        else if (textChar >= 65 && textChar <= 90) {
+          if ((textChar - offset) < 65 && (textChar - offset % 26) < 65) {
+            let differenceMayus = 65 - (textChar - offset % 26);
+            decodeResult += String.fromCharCode(91 - differenceMayus);
+          }
+          else decodeResult += String.fromCharCode(textChar - offset % 26);
+        }
       }
-    } else {
-      decodeResult += String.fromCharCode((textChar));
+      else decodeResult += String.fromCharCode(textChar);
+    }
+    return decodeResult;
+  },
+  createCipherWithOffset() {
+    return {
+      encode(offset,text) {
+        return text;
+      },
+      decode(offset, text) {
+        return text;
+      }
     }
   }
-  return decodeResult;
-}
+};
 
-function createCipherWithOffset() { }
+cipher.createCipherWithOffset(5);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
-var encodeText, decodeText, offset;
+let encodeText, decodeText, offset;
 
-document.getElementById("btnEncrypted").addEventListener("click", function () {
+document.getElementById("btnEncrypted").addEventListener("click", () => {
   offset = parseInt(document.getElementById("txtOffset").value);
   encodeText = document.getElementById("txtEncrypted").value;
   if (offset) {
     if (encodeText) {
       document.getElementById("answer").style.display = "block";
       document.getElementById("result").innerHTML = cipher.encode(offset, encodeText);
-    } else alert("No colocaste un texto para modificar");
-  } else alert("No colocaste un número de desplazamientos para modificar el texto");
+    }
+    else alert("No colocaste un texto para modificar");
+  }
+  else alert("No colocaste un número de desplazamientos para modificar el texto");
 });
 
-document.getElementById("btnDecrypted").addEventListener("click", function () {
+document.getElementById("btnDecrypted").addEventListener("click", () => {
   offset = parseInt(document.getElementById("txtOffset").value);
   decodeText = document.getElementById("txtEncrypted").value;
   if (offset) {
     if (decodeText) {
       document.getElementById("answer").style.display = "block";
       document.getElementById("result").innerHTML = cipher.decode(offset, decodeText);
-    } else alert("No colocaste un texto para modificar");
-  } else alert("No colocaste un número de desplazamientos para modificar el texto");
+    }
+    else alert("No colocaste un texto para modificar");
+  }
+  else alert("No colocaste un número de desplazamientos para modificar el texto");
 });
 
-document.getElementById("clear").addEventListener("click", function () {
+document.getElementById("clear").addEventListener("click", () => {
   document.getElementById("txtOffset").value = "";
   document.getElementById("txtEncrypted").value = "";
   document.getElementById("result").innerHTML = "";
