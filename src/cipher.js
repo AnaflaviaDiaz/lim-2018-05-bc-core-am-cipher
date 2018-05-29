fc963d1bee32ea5c4cfea6f2c6b8a8f194895b59
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-29 16:40:10 -0500

    subiendo porcentaje de test

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,40 @@
 window.cipher = {
   encode(offset, text) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let encodeResult = "";
 
     for (let i = 0; i < text.length; i++) {
       let textChar = text.charCodeAt(i);
-      let charter = text.charAt(i);
-
-      if (alphabet.includes(charter)) {
-        if (textChar >= 97 && textChar <= 122) {
-          encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
-        }
-        else if (textChar >= 65 && textChar <= 90) {
-          encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
-        }
+      if (textChar >= 97 && textChar <= 122) {
+        encodeResult += String.fromCharCode((textChar - 97 + offset) % 26 + 97);
       }
-      // else encodeResult += String.fromCharCode(textChar);
+      else if (textChar >= 65 && textChar <= 90) {
+        encodeResult += String.fromCharCode((textChar - 65 + offset) % 26 + 65);
+      }
+      else encodeResult += String.fromCharCode(textChar);
     }
 
     return encodeResult;
   },
   decode(offset, text) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let decodeResult = "";
 
     for (let i = 0; i < text.length; i++) {
       let textChar = text.charCodeAt(i);
-      let charter = text.charAt(i);
-
-      if (alphabet.includes(charter)) {
-        if (textChar >= 97 && textChar <= 122) {
-          if ((textChar - offset) < 97 && (textChar - offset % 26) < 97) {
-            let differenceMinus = 97 - (textChar - offset % 26);
-            decodeResult += String.fromCharCode(123 - differenceMinus);
-          }
-          else decodeResult += String.fromCharCode(textChar - offset % 26);
+      if (textChar >= 97 && textChar <= 122) {
+        if ((textChar - offset) < 97 && (textChar - offset % 26) < 97) {
+          let differenceMinus = 97 - (textChar - offset % 26);
+          decodeResult += String.fromCharCode(123 - differenceMinus);
         }
-        else if (textChar >= 65 && textChar <= 90) {
-          if ((textChar - offset) < 65 && (textChar - offset % 26) < 65) {
-            let differenceMayus = 65 - (textChar - offset % 26);
-            decodeResult += String.fromCharCode(91 - differenceMayus);
-          }
-          else decodeResult += String.fromCharCode(textChar - offset % 26);
-        }
+        else decodeResult += String.fromCharCode(textChar - offset % 26);
       }
-      // else decodeResult += String.fromCharCode(textChar);
+      else if (textChar >= 65 && textChar <= 90) {
+        if ((textChar - offset) < 65 && (textChar - offset % 26) < 65) {
+          let differenceMayus = 65 - (textChar - offset % 26);
+          decodeResult += String.fromCharCode(91 - differenceMayus);
+        }
+        else decodeResult += String.fromCharCode(textChar - offset % 26);
+      }
+      else decodeResult += String.fromCharCode(textChar);
     }
     return decodeResult;
   },
@@ -56,5 +46,3 @@ window.cipher = {
     return re;
   }
 };
-
-cipher.createCipherWithOffset(5);
