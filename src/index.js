b9dba24c721cf20d8c8d9e166690eee6061f3da2
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-05-30 19:13:45 -0500

    agregando warming y empezando con bootstrap

--- src/index.js
+++ src/index.js
@@ -7,22 +7,34 @@ document.getElementById("btnEncrypted").addEventListener("click", () => {
   offset = parseInt(document.getElementById("txtOffset").value);
   encodeText = document.getElementById("txtEncrypted").value;
   if (offset) {
+    document.getElementById("dangerOffset").style.display = "none";
     if (encodeText) {
+      document.getElementById("dangerText").style.display = "none";
       document.getElementById("answer").style.display = "block";
       document.getElementById("result").innerHTML = cipher.encode(offset, encodeText);
-    } else alert("No colocaste un texto para modificar");
-  } else alert("No colocaste un número de desplazamientos para modificar el texto");
+    } else {
+      document.getElementById("dangerText").style.display = "block";
+    }
+  } else {
+    document.getElementById("dangerOffset").style.display = "block";
+  }
 });
 
 document.getElementById("btnDecrypted").addEventListener("click", () => {
   offset = parseInt(document.getElementById("txtOffset").value);
   decodeText = document.getElementById("txtEncrypted").value;
   if (offset) {
+    document.getElementById("dangerOffset").style.display = "none";
     if (decodeText) {
+      document.getElementById("dangerText").style.display = "none";
       document.getElementById("answer").style.display = "block";
       document.getElementById("result").innerHTML = cipher.decode(offset, decodeText);
-    } else alert("No colocaste un texto para modificar");
-  } else alert("No colocaste un número de desplazamientos para modificar el texto");
+    } else {
+      document.getElementById("dangerText").style.display = "block";
+    }
+  } else {
+    document.getElementById("dangerOffset").style.display = "block";
+  }
 });
 
 document.getElementById("change").addEventListener("click", () => {
@@ -31,12 +43,12 @@ document.getElementById("change").addEventListener("click", () => {
     document.getElementById("txtEncrypted").value = result;
     document.getElementById("result").innerHTML = "";
     document.getElementById("answer").style.display = "none";
-  } else {
-    console.log("vacio result");
   }
 });
 
 document.getElementById("clear").addEventListener("click", () => {
+  document.getElementById("dangerOffset").style.display = "none";
+  document.getElementById("dangerText").style.display = "none";
   document.getElementById("txtOffset").value = "";
   document.getElementById("txtEncrypted").value = "";
   document.getElementById("result").innerHTML = "";
